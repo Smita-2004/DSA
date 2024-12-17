eab5d67e8a2ef6b6066c20e800cc9d772ea0df65
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2024-12-17 09:16:56 +0530

    Add files via upload

---
+++ mathematics/quadracticEquationRoot.java
@@ -0,0 +1,14 @@
+// Given a quadratic equation ax2 + bx + c = 0, find its roots. If the roots are imaginary, return only one integer -1. Always return the roots as the greatest integers less than or equal to the actual roots, with the maximum root first followed by the minimum root.
+// Note: If roots are imaginary, the generated output will display "Imaginary".
+// Examples:
+
+// Input:
+// a = 1, b = -2, c = 1
+// Output: 1 1
+// Explanation:
+// Roots of equation x2-2x+1 are 1 and 1.
+// Input:
+// a = 1, b = -7, c = 12
+// Output: 4 3
+// Explanation: Roots of equation x2 - 7x + 12 are 4 and 3.
+ 
