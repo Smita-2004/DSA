2a40ae385b3f0a9ce7d40f2b6225cb84ca8c1a4b
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2025-02-14 10:11:27 +0530

    Add files via upload

---
+++ Leetcode/16Jan/binarySearch.cpp
@@ -0,0 +1,22 @@
+// class Solution {
+//   public:
+//     int binarysearch(vector<int> &arr, int k) {
+//         // code here
+//         int low=0;
+//         int high=arr.size()-1;
+//         while(low<=high){
+//             int mid=(low+high)/2;
+//             if(arr[mid]==k){
+//                 return mid;
+//             }
+//             else if(arr[mid]<k){
+//                 low=mid+1;
+//             }
+//             else{
+//                 low=mid-1;
+//             }
+            
+//         }
+//         return -1;
+//     }
+  // };
