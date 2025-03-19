edaa9ad541194cf34e4e7c02e04958604d1727ac
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2025-03-19 08:03:47 +0530

    Add files via upload

---
+++ Leetcode/30Jan/LinkedList/calulateLength.cpp
@@ -0,0 +1,9 @@
+// int findLength(Node *head){
+//     int length = 0;
+//     Node *curr = head;
+//     while(curr != NULL{
+//         lenght++;
+//         curr = curr->next;
+//     }
+//     return length;
+// }
