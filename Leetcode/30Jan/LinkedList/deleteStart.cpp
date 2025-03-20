c21317c939a752a0747f5eef446b04edc24ad633
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2025-03-20 09:41:32 +0530

    Add files via upload

---
+++ Leetcode/30Jan/LinkedList/deleteStart.cpp
@@ -0,0 +1,10 @@
+// Node *removeFirstNode(Node *head){
+//     if(head == NULL{
+//         return NULL;
+//     }
+
+//     Node *temp = head;
+//     head = head->next;
+//     delete temp;
+//     return head;
+// }
