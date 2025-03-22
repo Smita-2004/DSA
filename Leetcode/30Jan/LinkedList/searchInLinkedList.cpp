6e1ab7d79210539905bb059faf459397295929d9
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2025-03-22 07:12:13 +0530

    Add files via upload

---
+++ Leetcode/30Jan/LinkedList/searchInLinkedList.cpp
@@ -0,0 +1,9 @@
+// bool searchLinkedList(struct Node *head, int traget){
+//     while(head !=NULL{
+//         if(head->data == target){
+//             return true;
+//         }
+//         head = head->next;
+//     }
+//     return false;
+// }
