8ed101acd22b6bcd6a4a874239bad5cd69f6a737
Smita Tiwari <114800220+Smita-2004@users.noreply.github.com>
2025-04-01 11:31:26 +0530

    Add files via upload

---
+++ Leetcode/10Feb/Dp/fibonacci.cpp
@@ -0,0 +1,21 @@
+// 509: leetcode
+
+// class Solution {
+//     public:
+//     int helper(int n, vector<int> &dp){
+//             if(n<=1) return n;
+//             if(dp[n]!=-1){
+//                 return dp[n];
+//             }
+//             return dp[n] = fib(n-1) + fib(n-2);
+    
+//     }
+//         int fib(int n) {
+//           vector<int> dp(n+1,-1);
+//           return helper(n,dp);
+//         }
+//     };
+    
+    
+    
+    
